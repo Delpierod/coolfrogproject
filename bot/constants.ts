3e73418c76c9552d82d209ceb8663487aa78b123
Delpierod <delpierod@outlook.com>
2025-01-25 13:39:39 +0300

    Initial commit

--- bot/constants.ts
+++ bot/constants.ts
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-export const PORT = Number(process.env.PORT) || 3000;
+export const PORT = Number(process.env.PORT) || 3001;
 export const APP_URL = String(process.env.APP_URL);
 export const TELEGRAM_BOT_TOKEN = String(process.env.TELEGRAM_BOT_TOKEN);
 export const API_URL = String(process.env.API_URL);
